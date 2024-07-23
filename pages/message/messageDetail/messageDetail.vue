e3a8e23dd4521d84f9b0c417c0cfa228b6dce205
fangql <2054963713@qq.com>
2024-07-23 13:12:01 +0800

    消息数据模型更加丰富

--- pages/message/messageDetail/messageDetail.vue
+++ pages/message/messageDetail/messageDetail.vue
@@ -1,14 +1,22 @@
 <template>
 	<view class="page">
-		<scroll-view class="scroll-view" scroll-y scroll-with-animation :scroll-top="top" >
+		<scroll-view class="scroll-view" scroll-y scroll-with-animation :scroll-top="top">
 			<view style="padding: 30rpx 30rpx 1rpx 30rpx;">
-				<view class="message" :class="[item.userType]" v-for="(item,index) in list" :key="index">
-					<image :src="item.avatar" v-if="item.userType === 'friend'" class="avatar" mode="widthFix"></image>
-					<view class="content" v-if="item.messageType === 'image'">
-						<image :src="item.content" mode="widthFix"></image>
+				<view class="message-card" v-for="(item,index) in list" :key="index">
+					<view class="timestamp">
+						{{convertUnixTimestampToDateString(item.timestamp)}}
 					</view>
-					<view class="content" v-else> {{ item.content }} </view>
-					<image :src="item.avatar" v-if="item.userType === 'self'" class="avatar" mode="widthFix"></image>
+					<view class="message" :class="[item.senderId]">
+						<view class="status" v-if="item.senderId === 'user_B'">
+							{{item.status}}
+						</view>
+						<image :src="avatar" v-if="item.senderId === 'user_A'" class="avatar" mode="widthFix"></image>
+						<view class="content" v-if="item.type === 'image'">
+							<image :src="item.media.url" mode="widthFix"></image>
+						</view>
+						<view class="content" v-else> {{ item.text }} </view>
+						<image :src="avatar" v-if="item.senderId === 'user_B'" class="avatar" mode="widthFix"></image>
+					</view>
 				</view>
 			</view>
 		</scroll-view>
@@ -23,7 +31,132 @@
 		data() {
 			return {
 				content: '',
-				list: [],
+				avatar: '/static/logo.png',
+				list: [{
+						id: "msg_001",
+						senderId: "user_A",
+						receiverId: "user_B",
+						text: "Hey there! How's your day?",
+						media: null,
+						timestamp: 1689877834000,
+						read: true,
+						deleted: false,
+						type: "text",
+						status: "seen"
+					},
+					{
+						id: "msg_002",
+						senderId: "user_B",
+						receiverId: "user_A",
+						text: "It's going well, thanks for asking!",
+						media: null,
+						timestamp: 1689878000000,
+						read: true,
+						deleted: false,
+						type: "text",
+						status: "seen"
+					},
+					{
+						id: "msg_003",
+						senderId: "user_A",
+						receiverId: "user_B",
+						text: null,
+						media: {
+							url: "/static/20240723124604.png",
+							type: "image/jpeg",
+							size: 123456
+						},
+						timestamp: 1689878100000,
+						read: true,
+						deleted: false,
+						type: "image",
+						status: "seen"
+					},
+					{
+						id: "msg_004",
+						senderId: "user_B",
+						receiverId: "user_A",
+						text: "Nice photo! Where did you take it?",
+						media: null,
+						timestamp: 1689878200000,
+						read: true,
+						deleted: false,
+						type: "text",
+						status: "seen"
+					},
+					{
+						id: "msg_005",
+						senderId: "user_A",
+						receiverId: "user_B",
+						text: "I took it on my last trip to the mountains.",
+						media: null,
+						timestamp: 1689878300000,
+						read: true,
+						deleted: false,
+						type: "text",
+						status: "seen"
+					},
+					{
+						id: "msg_006",
+						senderId: "user_B",
+						receiverId: "user_A",
+						text: "Sounds amazing! I'd love to see more photos.",
+						media: null,
+						timestamp: 1689878400000,
+						read: true,
+						deleted: false,
+						type: "text",
+						status: "seen"
+					},
+					{
+						id: "msg_007",
+						senderId: "user_A",
+						receiverId: "user_B",
+						text: "Sure thing, I'll send some more later.",
+						media: null,
+						timestamp: 1689878500000,
+						read: true,
+						deleted: false,
+						type: "text",
+						status: "seen"
+					},
+					{
+						id: "msg_008",
+						senderId: "user_B",
+						receiverId: "user_A",
+						text: "Great, looking forward to it!",
+						media: null,
+						timestamp: 1689878600000,
+						read: false,
+						deleted: false,
+						type: "text",
+						status: "delivered"
+					},
+					{
+						id: "msg_009",
+						senderId: "user_A",
+						receiverId: "user_B",
+						text: "By the way, have you seen any good movies lately?",
+						media: null,
+						timestamp: 1689878700000,
+						read: false,
+						deleted: false,
+						type: "text",
+						status: "sent"
+					},
+					{
+						id: "msg_010",
+						senderId: "user_B",
+						receiverId: "user_A",
+						text: "Yes, I just watched an excellent thriller. I can recommend it.",
+						media: null,
+						timestamp: 1689878800000,
+						read: false,
+						deleted: false,
+						type: "text",
+						status: "pending"
+					}
+				],
 				top: 0
 			};
 		},
@@ -31,19 +164,9 @@
 			uni.setNavigationBarTitle({
 				title: options.name
 			})
-			this._friendAvatar = '/static/logo.png'
-			this._selfAvatar = '/static/logo.png'
-			this.list = [{
-				content: '对方历史回复消息',
-				userType: 'friend',
-				avatar: this._friendAvatar
-			}, {
-				content: '历史消息',
-				userType: 'self',
-				avatar: this._selfAvatar
-			}]
+			//获取数据
 		},
-		
+
 		methods: {
 			send() {
 				this.list.push({
@@ -88,6 +211,16 @@
 			},
 			scrollToBottom() {
 				this.top = this.list.length * 1000
+			},
+			convertUnixTimestampToDateString(timestamp) {
+				return new Intl.DateTimeFormat('default', {
+					year: 'numeric',
+					month: '2-digit',
+					day: '2-digit',
+					hour: '2-digit',
+					minute: '2-digit',
+					second: '2-digit'
+				}).format(new Date(timestamp));
 			}
 		}
 	}
@@ -98,80 +231,99 @@
 		height: calc(100vh - 44px - 120rpx);
 		/* #endif */
 		/* #ifndef H5 */
-		height: 100vh  - 120rpx;
+		height: 100vh - 120rpx;
 		/* #endif */
 		background: #eee;
 		box-sizing: border-box;
 	}
 
-	.message {
-		display: flex;
-		align-items: flex-start;
-		margin-bottom: 30rpx;
-
-		.avatar {
-			width: 80rpx;
-			height: 80rpx;
-			border-radius: 10rpx;
-			margin-right: 30rpx;
-		}
-
-		.content {
-			box-sizing: border-box;
-			min-height: 80rpx;
-			max-width: 60vw;
-			padding: 20rpx;
-			border-radius: 10rpx;
+	.message-card {
+		.timestamp {
+			text-align: center;
+			margin-bottom: 13rpx;
 			font-size: 28rpx;
-			line-height: 1.3;
-			background: #fff;
-			overflow-wrap: break-word; /* 同上，但更现代的标准 */
-
-			image {
-				width: 200rpx;
-			}
 		}
 
-		&.self {
-			justify-content: flex-end;
+		.message {
+			display: flex;
+			align-items: flex-start;
+			margin-bottom: 30rpx;
+
+			.status {
+				font-size: 28rpx;
+				align-self: flex-end;
+				margin-right: 10rpx;
+			}
 
 			.avatar {
-				margin: 0 0 0 30rpx;
+				width: 80rpx;
+				height: 80rpx;
+				border-radius: 10rpx;
+				margin-right: 30rpx;
 			}
 
 			.content {
-				position: relative;
+				box-sizing: border-box;
+				min-height: 80rpx;
+				max-width: 60vw;
+				padding: 20rpx;
+				border-radius: 10rpx;
+				font-size: 28rpx;
+				line-height: 1.3;
+				background: #fff;
+				word-break: break-all;
+				/* 同上，但更现代的标准 */
 
-				&::after {
-					position: absolute;
-					content: '';
-					width: 0;
-					height: 0;
-					border: 16rpx solid transparent;
-					border-left: 16rpx solid #fff;
-					right: -28rpx;
-					top: 24rpx;
+				image {
+					width: 200rpx;
+				}
+			}
+
+
+			&.user_B {
+				justify-content: flex-end;
+
+				.avatar {
+					margin: 0 0 0 30rpx;
+				}
+
+				.content {
+					position: relative;
+
+					&::after {
+						position: absolute;
+						content: '';
+						width: 0;
+						height: 0;
+						border: 16rpx solid transparent;
+						border-left: 16rpx solid #fff;
+						right: -28rpx;
+						top: 24rpx;
+					}
+				}
+			}
+
+			&.user_A {
+				.content {
+					position: relative;
+
+					&::after {
+						position: absolute;
+						content: '';
+						width: 0;
+						height: 0;
+						border: 16rpx solid transparent;
+						border-right: 16rpx solid #fff;
+						left: -28rpx;
+						top: 24rpx;
+					}
 				}
 			}
 		}
 
-		&.friend {
-			.content {
-				position: relative;
-
-				&::after {
-					position: absolute;
-					content: '';
-					width: 0;
-					height: 0;
-					border: 16rpx solid transparent;
-					border-right: 16rpx solid #fff;
-					left: -28rpx;
-					top: 24rpx;
-				}
-			}
-		}
 	}
+
+
 
 	.tool {
 		position: fixed;
